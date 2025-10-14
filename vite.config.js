1e1787b4669aef320ba7f19f918f2292aed1b7b2
Filip-Povidernyi <p.poviderniy@gmail.com>
2025-10-14 12:11:58 +0300

    Changed Vite
    Changed Vite config for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // якщо твій репозиторій на GitHub має таку назву, зміни нижче:
 export default defineConfig({
   plugins: [react()],
-  base: '/campVehicles/',
+  base: '/',
 })
+// campVehicles/
